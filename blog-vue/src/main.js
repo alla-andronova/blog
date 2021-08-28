e0ce5201969f55d64fcf5a0836e8950a786653d1
Anna Shkajeva <anna.shkaeva@gmail.com>
2021-08-28 23:42:51 +0300

    store

--- blog-vue/src/main.js
+++ blog-vue/src/main.js
@@ -1,42 +1,7 @@
-// нет в дефолт
-//import Vue from 'vue'  
-
-import { createApp } from 'vue' //есть в дефолт
+import { createApp } from 'vue'
 import App from './App.vue'
-// import VueRouter from 'vue-router'
 import router from './router/index'
-import store from './store/store' // есть в дефолт
+import store from './store'
 
-// 
 
 createApp(App).use(store).use(router).mount('#app')
-
-
-// Vue.use(vue-router)
-
-
-// нет дефолт
-
-// new Vue({
-//     render: h => h(App), router, store,
-//     // el: 'app',
-//   }).$mount("#app")
-
-
-
-
-
-
-
-
-
-
-
-
-// manual features
-// - select only: vue version, router, eslint
-// - vue version 3.x
-// - use history with router?: y
-// - eslint with errors only
-// - eslint on save
-// - save as preset? n
---
+++ blog-vue/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+import state from "./state"
+
+
+export default createStore({
+    state,
+  });
+
+
+
+
+  // console.log(store.state.count) //need
+
+
+
